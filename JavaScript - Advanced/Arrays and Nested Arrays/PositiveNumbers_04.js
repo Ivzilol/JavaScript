1ecf618324089a2b6efaa9b476517ea0d4b39529
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-12-24 09:47:44 +0200

    JavaScript - Advanced

---
+++ JavaScript - Advanced/Arrays and Nested Arrays/PositiveNumbers_04.js
@@ -0,0 +1,16 @@
+function solve(nums) {
+    const result = [];
+
+    for(let num of nums) {
+        if (num < 0) {
+            result.unshift(num);
+        } else {
+            result.push(num);
+        }
+    }
+    console.log(result.join('\n'));
+}
+
+
+solve([7, -2, 8, 9]);
+solve([3, -2, 0, -1]);
