f1f91af842480874eba3e5f00828c370e50fc1ae
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-12-24 09:49:04 +0200

    JavaScript - Advanced

---
+++ JavaScript - Advanced/Arrays and Nested Arrays/PieceOfPie_07.js
@@ -0,0 +1,23 @@
+function solve(pies, start, end) {
+    const startIndex = pies.indexOf(start);
+    const endIndex = pies.indexOf(end) + 1;
+
+    return (pies.slice(startIndex, endIndex));
+}
+
+solve(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+);
+solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie');
