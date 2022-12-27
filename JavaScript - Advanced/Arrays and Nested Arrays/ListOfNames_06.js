08c0c3fbf1c8a725c3cf5ff6e224fc71cddc107d
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-12-27 19:56:45 +0200

    JavaScript

---
+++ JavaScript - Advanced/Arrays and Nested Arrays/ListOfNames_06.js
@@ -0,0 +1,9 @@
+function solve(array) {
+    array.sort((a, b) => a.localeCompare(b));
+    for (let index = 0; index < array.length; index++) {
+        console.log(`${index + 1}.${array[index]}`)
+        
+    }
+}
+
+solve(["John", "Bob", "Christina", "Ema"]);
