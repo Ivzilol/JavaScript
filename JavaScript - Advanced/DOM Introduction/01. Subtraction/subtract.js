65b8a344ca565b791aafcd1ad33b8e0f9d17a48a
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-04 20:40:30 +0200

    JavaScript

---
+++ JavaScript - Advanced/DOM Introduction/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+    result.textContent = Number(firstNumber) - Number(secondNumber);
+}
