6a22eb477708645b4c12c2a7e301bb63449a7408
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-05 18:51:26 +0200

    JavaScript

---
+++ JavaScript - Advanced/DOM Introduction/03. Accordion/accordion.js
@@ -0,0 +1,12 @@
+function toggle() {
+    let textElement = document.getElementById('extra');
+    let buttonElement = document.getElementsByClassName('button')[0];
+    if(buttonElement.textContent === "More") {
+        buttonElement.textContent = 'Less';
+        textElement.style.display = 'block';
+    } else {
+        buttonElement.textContent = 'More';
+        textElement.style.display = 'none';
+    }
+}
+
