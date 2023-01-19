3628353985c0fe7532d87e263a413c6f46a06c8d
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-19 23:06:28 +0200

    JavaScript

---
+++ JavaScript - Advanced/Advanced Functions/AreaAndVolumeCalculator_01.js
@@ -0,0 +1,34 @@
+function areaVol(areaIn, volIn, input) {
+    const data = JSON.parse(input);
+
+    return data.map(cube => {
+        return {
+            area: areaIn.call(cube),
+            volume: volIn.call(cube)
+        };
+    });
+}
+
+console.log(areaVol(area, vol, `[
+{"x":"1","y":"2","z":"10"},
+{"x":"7","y":"7","z":"10"},
+{"x":"5","y":"2","z":"10"}
+]`
+));
+console.log(areaVol(area, vol, `[
+{"x":"10","y":"-22","z":"10"},
+{"x":"47","y":"7","z":"-5"},
+{"x":"55","y":"8","z":"0"},
+{"x":"100","y":"100","z":"100"},
+{"x":"55","y":"80","z":"250"}
+]`
+));
+
+
+function area() {
+    return Math.abs(this.x * this.y);
+}
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+}
