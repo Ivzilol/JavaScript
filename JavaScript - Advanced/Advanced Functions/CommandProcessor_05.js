1bbe86d773ba5c5472514c3f39c3822177b3d1b8
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-19 23:07:20 +0200

    JavaScript

---
+++ JavaScript - Advanced/Advanced Functions/CommandProcessor_05.js
@@ -0,0 +1,32 @@
+function createProcessor() {
+    let state = '';
+
+    function append(str) {
+        state += str;
+    }
+    function removeStart(n) {
+        state = state.slice(n);
+    }
+    function removeEnd(n) {
+        state = state.slice(0, - n);
+    }
+
+    function print() {
+        console.log(state);
+    }
+
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    }
+}
+
+const firstZeroTest = createProcessor();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
