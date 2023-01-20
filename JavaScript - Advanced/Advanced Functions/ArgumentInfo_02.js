546be769a088b0c59e8e8e7b934024ca93cbde17
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-01-20 14:11:22 +0200

    JavaScript

---
+++ JavaScript - Advanced/Advanced Functions/ArgumentInfo_02.js
@@ -0,0 +1,20 @@
+function solve() {
+    const params = Array.from(arguments);
+    const types = {};
+    for (const arg of params) {
+        const  type = typeof arg;
+        console.log(`${type}: ${arg}`);
+
+        if (types[type] === undefined) {
+            types[type] = 0;
+        }
+        types[type]++;
+    }
+    const result = Object.entries(types).sort((a, b) => b[1] - a[1]);
+    for (const [type, count] of result) {
+        console.log(`${type} = ${count}`)
+    }
+}
+
+solve('cat', 42, function () { console.log('Hello world!'); });
+solve({ name: 'bob'}, 3.333, 9.999);
