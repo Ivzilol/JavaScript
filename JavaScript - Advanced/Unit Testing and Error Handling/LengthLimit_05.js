c6e87b5eaa918ddeed1f154680c6ced80e9d8af5
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-02-11 21:05:50 +0200

    JavaScript

---
+++ JavaScript - Advanced/Unit Testing and Error Handling/LengthLimit_05.js
@@ -0,0 +1,28 @@
+class Stringer {
+    constructor(string, length) {
+        this.innerString = string;
+        this.innerLength = length;
+    }
+    increase(num) {
+        this.innerLength += num;
+    }
+    decrease(num) {
+        if (this.innerLength - num < 0) {
+            this.innerLength = 0;
+        } else {
+            this.innerLength -= num;
+        }
+    }
+    toString() {
+        if (this.innerLength < this.innerString.length) {
+            return this.innerString.substring(0, this.innerLength) + '...';
+        } else {
+            return this.innerString;
+        }
+    }
+}
+
+
+let s = new Stringer("Viktor", 6);
+s.decrease(3)
+console.log(s.toString())
