fafb2bf120a45d70926842c518911b8a60cd2222
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-03-17 17:01:31 +0200

    Js

--- JavaScript - Advanced/Exercises_05_02_23/06. Phonebook/app.js
+++ JavaScript - Advanced/Exercises_05_02_23/06. Phonebook/app.js
@@ -1,60 +1,71 @@
-
 function attachEvents() {
-
-    document.querySelector('#refresh').addEventListener('click', displayComments);
-    document.querySelector('#submit').addEventListener('click', addComment);
-
+    document.querySelector('#btnLoad').addEventListener('click', load);
+    document.querySelector("#btnCreate").addEventListener('click', create);
 }
 
-let baseUrl = 'http://localhost:3030/jsonstore/messenger';
+let phoneBook = document.querySelector('#phonebook');
+let baseUrl = 'http://localhost:3030/jsonstore/phonebook';
 
-function displayComments() {
+function load() {
     fetch(baseUrl)
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Error');
-            }
-            return res.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            let textArea = document.querySelector('#messages');
-            let comments = [];
-            Object.values(data).forEach(u => comments.push(`${u.author}: ${u.content}`));
-            textArea.value = comments.join('\n')
+            phoneBook.replaceChildren();
+            Object.values(data).forEach(p => {
+                let liElement = document.createElement('li');
+                liElement.textContent = `${p.person}: ${p.phone}`;
+                let buttonDelete = document.createElement('button');
+                buttonDelete.textContent = 'Delete';
+                buttonDelete.setAttribute('id', p._id);
+                buttonDelete.addEventListener('click', remove);
+                liElement.appendChild(buttonDelete);
+                phoneBook.appendChild(liElement);
+            })
         })
-        .catch(error => alert(error.message));
 }
 
-function addComment() {
-    let authorName = document.querySelector('[name="author"]');
-    let content = document.querySelector('[name="content"]');
+function create() {
+    let name = document.querySelector('#person');
+    let phone = document.querySelector('#phone');
 
-    if (!authorName.value || !content.value) {
+    if (!name.value || !phone.value) {
         return;
     }
-
     fetch(baseUrl, {
         method: "POST",
         headers: {
             'Content-type': 'application/json'
         },
         body: JSON.stringify({
-            author: authorName.value.trim(),
-            content: content.value.trim()
+            person: name.value.trim(),
+            phone: phone.value.trim()
         })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Error creating new record!")
+        }
+        return response.json();
     })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Error creating new record');
-            }
-            return res.json();
-        })
         .catch(err => alert(err));
-    displayComments();
-    authorName.value = '';
-    content.value = '';
+    load();
+    name.value = '';
+    phone.value = '';
+
 }
 
+function remove(e) {
+    let currentId = e.target.id
+    if (e.target.textContent === 'Delete') {
+        fetch(`${baseUrl}/${currentId}`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                load();
+                return response.json();
+            })
+            .catch(err => alert(err.message));
+    }
 
+}
 
 attachEvents();
